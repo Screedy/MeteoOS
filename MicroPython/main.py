7c67a8e61ad48ffc29e7bab47840a31336e1ddb2
Screedy <screedy2k@gmail.com>
2023-11-12 17:44:49 +0100

    Update main.py
    Initial work on render_homepage function and the __main__ side of code.

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,11 +7,15 @@ from math import ceil
 from collections import OrderedDict
 import os
 
+
+# import graphics_text
+import graphics
+
 display = PicoGraphics(display=DISPLAY_PICO_DISPLAY,
                        pen_type=PEN_P4,
                        rotate=0)  # Create display object
 
-X_MAX, Y_MAX = display.get_bounds()
+X_MAX, Y_MAX = display.get_bounds()  # 240 x 135
 
 button_a = Button(12)
 button_b = Button(13)
@@ -38,7 +42,24 @@ def render_homepage(sensor_number, graph_interval):
     :param graph_interval: The selected graph interval. Either 1 (Hourly), 2 (Daily), 3 (Weekly) or 4 (Monthly).
     :return: None
     """
-    pass
+
+    clear_fast()
+    display.set_pen(WHITE)
+    graphics.draw_arrow(10, 100, 10, 0, display)
+    graphics.draw_arrow(10, 20, 10, 180, display)
+    display.text("SENS1", 30, 15, 240, 2)
+    graphics.draw_clock(30, 35, display)
+    display.text("10:43", 45, 34, 240, 2)   # TODO: draw time next to the clock
+    graphics.draw_thermometer(30, 63, display)
+    display.text("21°C", 45, 58, 240, 2)    # TODO: draw temperature next to the thermometer
+    graphics.draw_humidity(30, 85, display)
+    display.text("63%", 45, 83, 240, 2)     # TODO: draw humidity next to the humidity icon
+
+    display.line(100, 10, 100, 120, 2)
+
+    display.update()
+
+    print("Homepage rendered")
 
 
 if __name__ == "__main__":
@@ -49,3 +70,33 @@ if __name__ == "__main__":
         import setup
 
         setup.initial()
+
+    try:
+        fr_homepage = open("homepage_settings.txt", "r")
+    except OSError:
+        fw_homepage = open("homepage_settings.txt", "w")
+        fw_homepage.write("0\n0")
+        fw_homepage.close()
+        fr_homepage = open("homepage_settings.txt", "r")
+
+    last_sensor = int(fr_homepage.readline())
+    last_graph_interval = int(fr_homepage.readline())
+    fr_homepage.close()
+
+    render_homepage(last_sensor, last_graph_interval)
+
+    while True:
+        time.sleep(.15)
+
+        if button_x.read():
+            # TODO: change the graph interval
+            pass
+        if button_y.read():
+            # TODO: go to settings
+            pass
+        if button_a.read():
+            # TODO: change the sensor to previous one
+            pass
+        if button_b.read():
+            # TODO: change the sensor to next one
+            pass
