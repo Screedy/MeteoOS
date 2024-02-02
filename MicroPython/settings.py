443fe336e16c1ed29a04bb46cb73b611d4749ca6
Screedy <screedy2k@gmail.com>
2024-02-02 17:31:27 +0100

    Introduced redundant settings
    Added a settings loop that is its own and not in the main loop

--- MicroPython/settings.py
+++ MicroPython/settings.py
@@ -1,4 +1,6 @@
-from config import Colors, display as dp
+import time
+from config import *
+import page_elements
 
 
 class Settings:
@@ -20,9 +22,41 @@ class Settings:
         pass
 
 
+def render_settings():
+    """Renders the settings page."""
+
+    display = Display()
+
+    page_elements.clear_fast()
+    display().set_pen(Colors.WHITE)
+    page_elements.render_sensor_details(30, 1)
+    page_elements.render_nav_arrows(110)
+
+    display().line(101, 16, 101, 120, 3)
+
+    page_elements.render_settings_buttons()
+    page_elements.render_settings_items()
+
+    display().update()
+
+    # print("Settings rendered")
+
+
 def settings_loop():
     """The settings loop that runs when settings page is active."""
 
     while True:
-        pass
-    
+        if button_x.read():
+            pass
+
+        if button_y.read():
+            return
+
+        if button_a.read():
+            pass
+
+        if button_b.read():
+            pass
+
+        render_settings()
+        time.sleep(0.1)
