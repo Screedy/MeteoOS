fbb8818ed1268178d501377da4c27d69f143bf6c
Screedy <screedy2k@gmail.com>
2024-02-02 17:06:53 +0100

    Base structure for settings page

---
+++ MicroPython/settings.py
@@ -0,0 +1,28 @@
+from config import Colors, display as dp
+
+
+class Settings:
+    """Class for all options in the settings menu page."""
+
+    def __init__(self):
+        """Initializes the settings menu."""
+
+        pass
+
+    def add_sensor(self):
+        """Adds a sensor to the list of sensors."""
+
+        pass
+
+    def remove_sensor(self):
+        """Removes a sensor from the list of sensors."""
+
+        pass
+
+
+def settings_loop():
+    """The settings loop that runs when settings page is active."""
+
+    while True:
+        pass
+    
