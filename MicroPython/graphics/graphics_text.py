5ace650b9941285a5d17a0cf92876db675f9de5b
Screedy <screedy2k@gmail.com>
2024-02-04 18:56:29 +0100

    Moving graphics_text.py
    Moving to its own /graphics directory to be less cluttered

---
+++ MicroPython/graphics/graphics_text.py
@@ -0,0 +1,31 @@
+from config.config import Colors
+
+
+def text_ok(x: int, y: int, display, color: int = Colors.WHITE):
+    """Draws an OK text on the display at the given coordinates.
+    Uses the global display object.
+
+    :param x: The x coordinate of the button.
+    :param y: The y coordinate of the button.
+    :param display: The display object where the text will be drawn.
+    :param color: The color of the button. Uses the pico_graphics create_pen() function to create a color.
+    :return: None
+    """
+
+    display.set_pen(color)
+    display.text("OK", x, y, 20, 1)
+
+
+def text_help(x: int, y: int, display, color: int = Colors.WHITE):
+    """Draws a help text on the display at the given coordinates.
+    Uses the global display object.
+
+    :param x: The x coordinate of the button.
+    :param y: The y coordinate of the button.
+    :param display: The display object where the text will be drawn.
+    :param color: The color of the button. Uses the pico_graphics create_pen() function to create a color.
+    :return: None
+    """
+
+    display.set_pen(color)
+    display.text("HELP", x, y, 40, 1)
