b210c8526f63c8932288c3c2ae3127eb4c361b10
Screedy <screedy2k@gmail.com>
2024-02-09 16:57:11 +0100

    Update brightness.py

--- MicroPython/pages/brightness.py
+++ MicroPython/pages/brightness.py
@@ -8,25 +8,32 @@ def render_brightness():
     """Renders the page for changing the brightness of the display."""
 
     display = Display()
-    brightness = load_brightness()
+    brightness = load_brightness() * 10
 
     page_elements.clear_fast()
     display().set_pen(Colors.WHITE)
     display().text("Change brightness", 2, 0, 236, 2)
-    display().text("Press A to increase brightness", 2, 20, 236, 2)
-    display().text("Press B to decrease brightness", 2, 40, 236, 2)
-    display().text("Press Y to confirm", 2, display.y_max - 20, 236, 2)
 
     render_nice_slider(brightness)
 
     display().update()
 
     while not button_y.read():
+        page_elements.clear_fast()
+        display().set_pen(Colors.WHITE)
+        display().text("Change brightness", 2, 0, 236, 2)
+        render_nice_slider(brightness)
+
         if button_a.read():
-            brightness = (brightness + 0.1) if brightness < 1 else 1
+            if brightness < 10:
+                brightness = brightness + 1
+            print(brightness)
         elif button_b.read():
-            brightness = (brightness - 0.1) if brightness > 0 else 0
-        display().set_brightness(brightness)
+            if brightness > 0:
+                brightness = brightness - 1
+            print(brightness)
+        display().set_backlight(brightness/10)
+        display().update()
 
 
 def load_brightness():
@@ -35,7 +42,7 @@ def load_brightness():
     with open("settings.txt", "r") as fr:
         for line in fr:
             if line.startswith("brightness"):
-                return line.split(":")[1]
+                return float(line.split(":")[1])
 
     return 0.5
 
@@ -51,14 +58,9 @@ def render_nice_slider(brightness):
     display().set_pen(Colors.WHITE)
     display().text("0.1", 10, 58, 236, 2)
     display().text("1.0", 205, 58, 236, 2)
-    for i in range(0, brightness*10):
+    for i in range(0, brightness):
         display().line(50+i*15, 60, 50+i*15, 70, 10)
 
 
 if __name__ == "__main__":
-    render_nice_slider(0.5)
-
-    display = Display()
-
-    display().update()
-
+    render_brightness()
