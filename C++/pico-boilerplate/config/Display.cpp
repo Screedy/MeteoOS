362387b0c40273b5931eae0fe29dfd24c10b9e7b
Screedy <screedy2k@gmail.com>
2024-03-13 22:31:03 +0100

    Changed to behavior of singleton

--- C++/pico-boilerplate/config/Display.cpp
+++ C++/pico-boilerplate/config/Display.cpp
@@ -1,22 +1,22 @@
 //
-// Created by Petr Jurásek on 11.03.2024.
+// Created by Petr Jurásek on 13.03.2024.
 //
 
 #include "Display.h"
 
-Display& Display::get_instance() {
+Display::Display()
+    :st7789(PicoDisplay::WIDTH, PicoDisplay::HEIGHT, ROTATE_0, false, get_spi_pins(BG_SPI_FRONT)),
+    graphics(st7789.width, st7789.height, nullptr){}
+
+Display& Display::getInstance() {
     static Display instance;
     return instance;
 }
 
-Display::Display()
-    :st7789(PicoDisplay::WIDTH, PicoDisplay::HEIGHT, ROTATE_0, false, get_spi_pins(BG_SPI_FRONT)),
-    graphics(st7789.width, st7789.height, nullptr) {}
-
-ST7789* Display::get_display() {
-    return &st7789;
+ST7789& Display::getDriver() {
+    return st7789;
 }
 
-PicoGraphics_PenRGB332* Display::get_graphics() {
-    return &graphics;
+PicoGraphics_PenRGB332& Display::getGraphics() {
+    return graphics;
 }
--- C++/pico-boilerplate/config/Display.h
+++ C++/pico-boilerplate/config/Display.h
@@ -1,5 +1,5 @@
 //
-// Created by Petr Jurásek on 11.03.2024.
+// Created by Petr Jurásek on 13.03.2024.
 //
 
 #ifndef PICO_BOILERPLATE_DISPLAY_H
@@ -8,27 +8,21 @@
 #include "drivers/st7789/st7789.hpp"
 #include "libraries/pico_graphics/pico_graphics.hpp"
 #include "pico_display.hpp"
+#include "pico/stdlib.h"
 
 using namespace pimoroni;
 
-/*
- * The Display singleton class is responsible for managing the only display object in the system.
- * It is a wrapper around the Pimoroni PicoDisplay library.
- */
 class Display {
-private:
-    ST7789 st7789;
-    PicoGraphics_PenRGB332 graphics;
-
-    Display();
-
+public:
+    static Display& getInstance();
+    ST7789& getDriver();
+    PicoGraphics_PenRGB332& getGraphics();
     Display(const Display&) = delete;
     Display& operator=(const Display&) = delete;
-
-public:
-    static Display& get_instance();
-    ST7789* get_display();
-    PicoGraphics_PenRGB332* get_graphics();
+private:
+    Display();
+    ST7789 st7789;
+    PicoGraphics_PenRGB332 graphics;
 };
 
 #endif //PICO_BOILERPLATE_DISPLAY_H
