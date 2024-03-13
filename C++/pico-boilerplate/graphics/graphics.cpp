922a15e3afdf09484b73bababf880747e829d441
Screedy <screedy2k@gmail.com>
2024-03-13 22:30:01 +0100

    Changed to used files.

---
+++ C++/pico-boilerplate/graphics/graphics.cpp
@@ -0,0 +1,126 @@
+//
+// Created by Petr Jurásek on 13.03.2024.
+//
+
+#include "graphics.h"
+
+namespace Graphics{
+    Display& display = Display::getInstance();
+    pimoroni::ST7789& driver = display.getDriver();
+    pimoroni::PicoGraphics_PenRGB332& graphics = display.getGraphics();
+}
+
+using namespace Graphics;
+
+pimoroni::Point rotate_point(pimoroni::Point rotatedPoint, pimoroni::Point center, int angle) {
+    pimoroni::Point result;
+
+    int x = rotatedPoint.x;
+    int y = rotatedPoint.y;
+    int cx = center.x;
+    int cy = center.y;
+    float rad = angle * M_PI / 180;
+    int new_x = (int) (cos(rad) * (x - cx) - sin(rad) * (y - cy) + cx);
+    int new_y = (int) (sin(rad) * (x - cx) + cos(rad) * (y - cy) + cy);
+    result = pimoroni::Point{new_x, new_y};
+    return result;
+}
+
+std::array<pimoroni::Point, 3> createTriangle(int x, int y, int size){
+    std::array<pimoroni::Point, 3> triangle = {
+            pimoroni::Point{x, y},
+            pimoroni::Point{x + size, y},
+            pimoroni::Point{x + size / 2, y + size}
+    };
+    return triangle;
+}
+
+void draw_arrow(int x, int y, int size, int direction){
+    //Display &display = Display::get_instance();
+    //PicoGraphics_PenRGB332& graphics = *display.get_graphics();
+
+    graphics.set_pen(Colors::WHITE);
+    auto triangle = createTriangle(x, y, size);
+    for (int i = 0; i < 3; i++){
+        triangle[i] = rotate_point(triangle[i], pimoroni::Point{x + size / 2, y + size / 2}, direction);
+    }
+
+    graphics.line({triangle[0].x, triangle[0].y},{triangle[1].x, triangle[1].y});
+    graphics.line({triangle[1].x, triangle[1].y}, {triangle[2].x, triangle[2].y});
+    graphics.line({triangle[2].x, triangle[2].y}, {triangle[0].x, triangle[0].y});
+}
+
+void draw_empty_circle(int x, int y, int radius){
+    //Display &display = Display::get_instance();
+    //PicoGraphics_PenRGB332& graphics = *display.get_graphics();
+
+    graphics.set_pen(Colors::WHITE);
+    graphics.circle({x, y}, radius);
+    graphics.set_pen(Colors::BLACK);
+    graphics.circle({x, y}, radius - 1);
+}
+
+void draw_clock(int x, int y){
+    //Display &display = Display::get_instance();
+    //PicoGraphics_PenRGB332& graphics = *display.get_graphics();
+
+    draw_empty_circle(x, y, 10);
+    draw_empty_circle(x, y, 9);
+    graphics.set_pen(Colors::WHITE);
+    graphics.line({x, y}, {x, y - 7});
+    graphics.line({x, y}, {x + 4, y});
+}
+
+void draw_thermometer(int x, int y){
+    //Display &display = Display::get_instance();
+    //PicoGraphics_PenRGB332& graphics = *display.get_graphics();
+
+    draw_empty_circle(x, y + 5, 5);
+
+    graphics.line({x - 2, y}, {x + 3, y});
+    graphics.set_pen(Colors::WHITE);
+    graphics.line({x, y - 6}, {x, y + 5});
+
+    graphics.line({x - 2, y + 1}, {x - 2, y - 7});
+    graphics.line({x + 2, y + 1}, {x + 2, y - 7});
+
+    graphics.pixel({x - 1, y - 8});
+    graphics.pixel({x + 1, y - 8});
+
+    graphics.pixel({x, y - 9});
+
+    graphics.circle({x, y + 5}, 2);
+
+    graphics.line({x + 5, y - 8}, {x + 8, y - 8});
+    graphics.line({x + 5, y - 6}, {x + 7, y - 6});
+    graphics.line({x + 5, y - 4}, {x + 8, y - 4});
+    graphics.line({x + 5, y - 2}, {x + 7, y - 2});
+}
+
+void draw_humidity(int x, int y){
+    //Display &display = Display::get_instance();
+    //PicoGraphics_PenRGB332& graphics = *display.get_graphics();
+
+    graphics.set_pen(Colors::WHITE);
+    graphics.line({x, y - 9}, {x, y + 9});
+
+    graphics.line({x - 1, y - 7}, {x - 1, y + 9});
+    graphics.line({x - 2, y - 4}, {x - 2, y + 9});
+    graphics.line({x - 3, y - 2}, {x - 3, y + 8});
+    graphics.line({x - 4, y}, {x - 4, y + 7});
+    graphics.line({x - 5, y + 1}, {x - 5, y + 6});
+
+    graphics.line({x + 1, y - 7}, {x + 1, y + 9});
+    graphics.line({x + 2, y - 4}, {x + 2, y + 9});
+    graphics.line({x + 3, y - 2}, {x + 3, y + 8});
+    graphics.line({x + 4, y}, {x + 4, y + 7});
+    graphics.line({x + 5, y + 1}, {x + 5, y + 6});
+}
+
+void clear_fast(){
+    //Display &display = Display::get_instance();
+    //PicoGraphics_PenRGB332& graphics = *display.get_graphics();
+
+    graphics.set_pen(Colors::BLACK);
+    graphics.clear();
+}
---
+++ C++/pico-boilerplate/graphics/graphics.h
@@ -0,0 +1,97 @@
+//
+// Created by Petr Jurásek on 13.03.2024.
+//
+
+#ifndef PICO_BOILERPLATE_GRAPHICS_H
+#define PICO_BOILERPLATE_GRAPHICS_H
+
+#include "../config/Display.h"
+#include "../config/config.h"
+
+namespace Graphics{
+    extern Display& display;
+    extern pimoroni::ST7789& driver;
+    extern pimoroni::PicoGraphics_PenRGB332& graphics;
+}
+
+/*
+ * Rotates a point around the center point with a given angle.
+ *
+ * @param point The point to rotate. This is a tuple <x, y>.
+ * @param center The center of rotation. This is a tuple <x, y>.
+ * @param angle The angle of rotation in degrees.
+ * @return The rotated point. This is a tuple <x, y>.
+ */
+pimoroni::Point rotate_point(pimoroni::Point rotatedPoint, pimoroni::Point center, int angle);
+
+/*
+ * Creates a triangle with the given size and position. This function returns
+ * a sequence of points that represent the triangle.
+ *
+ * @param x The x coordinate of the triangle.
+ * @param y The y coordinate of the triangle.
+ * @param size The size of the triangle.
+ * @return A sequence of points that represent the triangle.
+ */
+std::array<pimoroni::Point, 3> createTriangle(int x, int y, int size);
+
+/*
+ * Draws an arrow on the display.
+ *
+ * @param x The x coordinate of the arrow.
+ * @param y The y coordinate of the arrow.
+ * @param size The size of the arrow.
+ * @param direction The direction of the arrow. This is an angle in degrees.
+ * @return None
+ *
+ * Example:
+ * @code
+ * draw_arrow(50, 50, 10, 0); // Draws an arrow pointing to the right.
+ * draw_arrow(50, 50, 10, 90); // Draws an arrow pointing up.
+ * @endcode
+ */
+void draw_arrow(int x, int y, int size, int direction);
+
+/*
+ * Draws an empty circle on the display.
+ * @param x The x coordinate of the circle.
+ * @param y The y coordinate of the circle.
+ * @param radius The radius of the circle.
+ * return None
+ */
+void draw_empty_circle(int x, int y, int radius);
+
+/*
+ * Draws a clock icon on the display.
+ *
+ * @param x The x coordinate of the clock.
+ * @param y The y coordinate of the clock.
+ * @return None
+ */
+void draw_clock(int x, int y);
+
+/*
+ * Draws a thermometer icon on the display.
+ *
+ * @param x The x coordinate of the thermometer.
+ * @param y The y coordinate of the thermometer.
+ * @return None
+ */
+void draw_thermometer(int x, int y);
+
+/*
+ * Draws a humidity icon on the display.
+ *
+ * @param x The x coordinate of the humidity.
+ * @param y The y coordinate of the humidity.
+ * @return None
+ */
+void draw_humidity(int x, int y);
+
+/*
+ * Clears the display without refreshing the display to remove flickering.
+ * @return None
+ */
+void clear_fast();
+
+#endif //PICO_BOILERPLATE_GRAPHICS_H
