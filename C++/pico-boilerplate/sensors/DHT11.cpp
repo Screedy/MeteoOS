3c67ec268fbea8b3708366a57bb4b74c46575991
Screedy <screedy2k@gmail.com>
2024-03-30 18:52:25 +0100

    Creation of DHT11 module class

---
+++ C++/pico-boilerplate/sensors/DHT11.cpp
@@ -0,0 +1,83 @@
+//
+// Created by Petr Jurásek on 19.03.2024.
+//
+
+#include "DHT11.h"
+
+DHT11::DHT11(unsigned int pin, std::string name, int interval)
+    : pin(pin), interval(interval), temperature(0), humidity(0) {
+
+    gpio_init(this->pin);
+
+    //Copy first 6 characters of the name and add null terminator
+    this->name = name.substr(0, 6);
+    this->name += '\0';
+
+    if (interval < 0) {
+        this->write = true;
+    } else {
+        this->write = false;
+    }
+}
+
+std::uint8_t DHT11::readByte() {
+    std::uint8_t result = 0;
+    for (int i = 7; i >= 0; i--){
+        waitForChange(1, 50);
+        sleep_us(30);
+        if (gpio_get(this->pin) == 1){
+            result |= (1 << i);
+            waitForChange(0, 50);
+        }
+    }
+    return result;
+}
+
+int DHT11::read() {
+
+    // Send start signal
+    gpio_set_dir(this->pin, GPIO_OUT);
+    gpio_put(this->pin, 0);
+    sleep_ms(20);
+    gpio_put(this->pin, 1);
+    gpio_set_dir(this->pin, GPIO_IN);
+    gpio_pull_up(this->pin);
+
+    // Wait for the sensor to respond
+    if (!waitForChange(0, 40)) return -1;
+    if (!waitForChange(1, 90)) return -1;
+    if (!waitForChange(0, 90)) return -1;
+
+    // Read the data
+    std::uint8_t humI = readByte();
+    std::uint8_t humF = readByte();
+    std::uint8_t tempI = readByte();
+    std::uint8_t tempF = readByte();
+    std::uint8_t checksum = readByte();
+
+    // Check the checksum
+    if (checksum != (humI + humF + tempI + tempF)) return -1;
+
+    // Store the data
+    this->humidity = humI + (humF / 100.0F);
+    this->temperature = tempI + (tempF / 100.0F);
+
+    return 0;
+}
+
+bool DHT11::waitForChange(bool state, unsigned int timeout_us){
+    auto start = time_us_32();
+    while (gpio_get(this->pin) != state) {
+        if (time_us_32() - start > timeout_us) return false;
+        tight_loop_contents();
+    }
+    return true;
+}
+
+float DHT11::getTemperature() {
+    return this->temperature;
+}
+
+float DHT11::getHumidity() {
+    return this->humidity;
+}
---
+++ C++/pico-boilerplate/sensors/DHT11.h
@@ -0,0 +1,51 @@
+//
+// Created by Petr Jurásek on 19.03.2024.
+//
+
+#ifndef PICO_BOILERPLATE_DHT11_H
+#define PICO_BOILERPLATE_DHT11_H
+
+#include <chrono>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
+#include <cstring>
+#include <string>
+
+/*
+ * The DHT11 class is responsible for reading the temperature and humidity from the DHT11 sensor.
+ * It creates a new instance of the sensor and reads the data from it.
+ *
+ * The sensor is read every interval seconds and the data is stored in the temperature and humidity fields.
+ *
+ * @param pin - the GPIO pin the sensor is connected to.
+ * @param name - the name of the sensor.
+ * @param interval - the interval in seconds at which the sensor is read. If the interval is less than 0,
+ * the sensor won't be read automatically and the read() function has to be called manually.
+ *
+ * @function read() - reads the data from the sensor and stores it in the temperature and humidity fields.
+ * @function getTemperature() - returns the temperature read from the sensor.
+ * @function getHumidity() - returns the humidity read from the sensor.
+ *
+ *
+ */
+class DHT11 {
+private:
+    unsigned int pin;
+    std::string name;
+    int interval;
+    float temperature;
+    float humidity;
+    bool write;
+
+    bool waitForChange(bool state, unsigned int timeout_us);
+    std::uint8_t readByte();
+
+public:
+    DHT11(unsigned int pin, std::string name, int interval);
+    int read();
+    float getTemperature();
+    float getHumidity();
+};
+
+#endif //PICO_BOILERPLATE_DHT11_H
