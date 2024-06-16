d62fbfd4710b17aca25d75ee41003de2810a565c
Screedy <screedy2k@gmail.com>
2024-06-16 22:53:27 +0200

    tweaks to add_sensor
    changed few things to work better, integration with getting all pins that are still available

--- C++/pico-boilerplate/pages/add_sensor.cpp
+++ C++/pico-boilerplate/pages/add_sensor.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "add_sensor.h"
+#include "../sensors/SensorType.h"
 
 namespace AddSensor{
     Display& display = Display::getInstance();
@@ -42,7 +43,7 @@ bool render_add_sensor() {
 
     if (selected_sensor == SensorType::DHT11) {
         sensor_manager.addSensor(std::make_unique<DHT11>(DHT11(selected_pin,
-                                                               sensor_name, selected_interval)))
+                                                               sensor_name, selected_interval)));
     }
 
     return true;
@@ -53,13 +54,15 @@ SensorType select_sensor_type() {
     int sensor_list_length = 1;//TODO: sensor_list_length
     int selected_sensor_index = 0;
 
+    sleep_ms(200);
+
     while (true){
         if (buttons.is_button_a_pressed()){
             selected_sensor_index = (selected_sensor_index + 1) % sensor_list_length;
         } else if (buttons.is_button_b_pressed()){
             selected_sensor_index = (selected_sensor_index - 1) % sensor_list_length;
         } else if (buttons.is_button_x_pressed()){
-            return SensorType::static_cast<SensorType>(selected_sensor_index);
+            return static_cast<SensorType>(selected_sensor_index);
         } else if (buttons.is_button_y_pressed()){
             return SensorType::UNKNOWN;
         }
@@ -67,7 +70,7 @@ SensorType select_sensor_type() {
         clear_fast();
         graphics.set_pen(Colors::WHITE);
         std::string text = "Select the sensor type: " +
-                sensor_type_to_string(SensorType::static_cast<SensorType>(selected_sensor_index));
+                sensor_type_to_string(static_cast<SensorType>(selected_sensor_index));
         graphics.text(text.c_str(), Point{2, 20}, 200, 2);
         driver.update(&graphics);
         sleep_ms(150);
@@ -75,7 +78,33 @@ SensorType select_sensor_type() {
 }
 
 int select_sensor_pin() {
+    std::vector<int> pin_list = sensor_manager.getAvailablePins();
+    int pin_list_length = pin_list.size();
+    int selected_pin_index = 0;
 
+    sleep_ms(200);
+
+    while (true){
+        if (buttons.is_button_a_pressed()) {
+            selected_pin_index = (selected_pin_index + 1) % pin_list_length;
+        } else if (buttons.is_button_b_pressed()) {
+            if (selected_pin_index == 0) { //C++ modulo operator does not work with negative numbers has to be handled this way
+                selected_pin_index = pin_list_length;
+            }
+            selected_pin_index = selected_pin_index - 1;
+        } else if (buttons.is_button_x_pressed()) {
+            return selected_pin_index;
+        } else if (buttons.is_button_y_pressed()) {
+            return -1;
+        }
+
+        clear_fast();
+        graphics.set_pen(Colors::WHITE);
+        std::string text = "Select the pin: " + std::to_string(pin_list[selected_pin_index]);
+        graphics.text(text.c_str(), Point{2, 20}, 200, 2);
+        driver.update(&graphics);
+        sleep_ms(150);
+    }
 }
 
 std::string select_sensor_name() {
@@ -83,19 +112,24 @@ std::string select_sensor_name() {
 
     auto current_ascii = 65;
 
+    sleep_ms(200);
+
     while (true){
         if (buttons.is_button_a_pressed()) {
             current_ascii = (current_ascii + 1) % 91;
-            if (current_ascii == 0):
-            current_ascii = 65;
+            if (current_ascii == 0) {
+                current_ascii = 65;
+            }
         } else if (buttons.is_button_b_pressed()) {
             current_ascii = (current_ascii - 1) % 91;
-            if (current_ascii == 64):
-            current_ascii = 90;
+            if (current_ascii == 64) {
+                current_ascii = 90;
+            }
         } else if (buttons.is_button_x_pressed()) {
-            if (name.length() < 6):
-            name += current_ascii;
-            else {
+            if (name.length() < 6) {
+                name += current_ascii;
+                current_ascii = 65;
+            }else {
                 return name;
             }
         } else if (buttons.is_button_y_pressed()) {
@@ -111,7 +145,7 @@ std::string select_sensor_name() {
         graphics.text("Name of the sensor:", Point{2, 20}, 200, 2);
         graphics.text(name.c_str(), Point{2, 40}, 200, 2);
         auto length = graphics.measure_text(name.c_str()) + 2;
-        graphics.text(char(current_ascii), Point{length, 40}, 200, 2);
+        graphics.text(std::string(1, current_ascii), Point{length, 40}, 200, 2);
 
         driver.update(&graphics);
         sleep_ms(150);
@@ -119,5 +153,26 @@ std::string select_sensor_name() {
 }
 
 int select_interval() {
+    int interval_time = 0;
 
+    graphics.set_pen(Colors::WHITE);
+
+    while (true){
+        if (buttons.is_button_a_pressed()) {
+            interval_time = (interval_time + 1) % 61;
+        } else if (buttons.is_button_b_pressed()) {
+            interval_time = (interval_time - 1) % 61;
+        } else if (buttons.is_button_x_pressed()) {
+            return interval_time;
+        } else if (buttons.is_button_y_pressed()) {
+            return -1;
+        }
+
+        clear_fast();
+        graphics.set_pen(Colors::WHITE);
+        std::string text = "Select the interval: " + std::to_string(interval_time);
+        graphics.text(text.c_str(), Point{2, 20}, 200, 2);
+        driver.update(&graphics);
+        sleep_ms(150);
+    }
 }
