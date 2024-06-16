2500f4f98ed43f887dfb07c883c564c7ab793036
Screedy <screedy2k@gmail.com>
2024-06-16 18:19:35 +0200

    Sensor types

--- C++/pico-boilerplate/sensors/Sensor.cpp
+++ C++/pico-boilerplate/sensors/Sensor.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "Sensor.h"
+
+std::string sensor_type_to_string(SensorType type) {
+    switch (type) {
+        case SensorType::DHT11: return "DHT11";
+        case SensorType::UNKNOWN: return "UNKNOWN";
+        default: return "UNKNOWN";
+    }
+}
--- C++/pico-boilerplate/sensors/Sensor.h
+++ C++/pico-boilerplate/sensors/Sensor.h
@@ -5,12 +5,23 @@
 #ifndef PICO_BOILERPLATE_SENSOR_H
 #define PICO_BOILERPLATE_SENSOR_H
 
+#include <string>
+
 class Sensor {
 public:
     virtual ~Sensor() {}
     int virtual read() = 0;
     float virtual getHumidity() = 0;
     float virtual getTemperature() = 0;
+    std::string virtual getName() = 0;
 };
+
+enum class SensorType {
+    DHT11,
+    UNKNOWN
+};
+
+std::string sensor_type_to_string(SensorType type);
+
 
 #endif //PICO_BOILERPLATE_SENSOR_H
