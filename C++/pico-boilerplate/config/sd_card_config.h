057f745163fd286936a4de87f0dc7877ab52c21d
Screedy <screedy2k@gmail.com>
2024-06-20 23:21:43 +0200

    new includes

--- C++/pico-boilerplate/config/sd_card_config.h
+++ C++/pico-boilerplate/config/sd_card_config.h
@@ -6,7 +6,8 @@
 #define PICO_BOILERPLATE_SD_CARD_CONFIG_H
 
 #include "ff.h"
-#include "sd_card.h"
+#include "diskio.h"
+#include "sd_card_manager.h"
 
 size_t sd_get_num();
 sd_card_t *sd_get_by_num(size_t num);
