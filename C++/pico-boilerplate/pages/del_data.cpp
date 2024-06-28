c8d3adddca0805c8e7710493fa424ae546fb9301
Screedy <screedy2k@gmail.com>
2024-06-28 17:28:26 +0200

    Bug fixes, changes to visuals
    Changed how the entire screen looks. Should now be fullscreen with colorful descriptions next to X and Y buttons.
    
    Added a condition if the directory doesnt exist while opening this page to create the directory.
    
    swapped the logic for a and b buttons to be visually correct.
    
    Added a "No files to delete" text on screen to represent a situation where no file is present.

--- C++/pico-boilerplate/pages/del_data.cpp
+++ C++/pico-boilerplate/pages/del_data.cpp
@@ -9,16 +9,17 @@
 #include "../sensors/SensorManager.h"
 #include "../config/sd_card_manager.h"
 #include "../graphics/page_elements.h"
-/*
+
 bool del_data(int selected_sensor_index){
     SensorManager& sensor_manager = SensorManager::getInstance();
-    sd_card_manager& sd_card_manager = sd_card_manager::getInstance();
+    sd_card_manager* sd_card_manager = sd_card_manager::get_instance();
 
     // Get the correct sensor from sensor manager
     std::unique_ptr<Sensor>& selected_sensor = sensor_manager.getSensor(selected_sensor_index);
 
     // Path to the sensor data file
-    const char* path = "0:/measurements/" + selected_sensor->getName() + ".txt";
+    std::string path_str = "0:/measurements/" + selected_sensor->getName() + ".txt";
+    const char* path = path_str.c_str();
 
     // Delete the file
     FRESULT fr = f_unlink(path);
@@ -29,15 +30,16 @@ bool del_data(int selected_sensor_index){
 
     return true;
 }
- */
 
 void render_del_data(){
     Display& display = Display::getInstance();
     pimoroni::ST7789& driver = display.getDriver();
     pimoroni::PicoGraphics_PenRGB332& graphics = display.getGraphics();
 
-    sd_card_manager& sd_card_manager = sd_card_manager::getInstance();
+    sd_card_manager* sd_card_manager = sd_card_manager::get_instance();
     SensorManager& sensor_manager = SensorManager::getInstance();
+
+    Buttons& buttons = Buttons::getInstance();
 
     const char* path = "0:/measurements/"; // Path to the folder with sensor data
 
@@ -46,8 +48,18 @@ void render_del_data(){
     FILINFO fno;
     FRESULT fr = f_opendir(&dir, path);
     if(fr != FR_OK){
-        printf("Failed to open directory %s\n", path);
-        return;
+        if (fr == FR_NO_PATH){
+            printf("Directory %s does not exist\n", path);
+            // Create the directory
+            fr = f_mkdir(path);
+            f_closedir(&dir);
+            return render_del_data();
+        } else {
+            printf("Failed to open directory %s\n", path);
+            printf("Error code: %d\n", fr);
+            f_closedir(&dir);
+            return;
+        }
     }
 
     // Save the file names to a vector
@@ -59,24 +71,27 @@ void render_del_data(){
         }
         files.push_back(fno.fname);
     }
-
-    // Close the directory
     f_closedir(&dir);
 
     int file_index = 0;
     int file_count = files.size();
 
     while(true){
+        sleep_ms(250);
         // Check for button presses
-        if (buttons.is_button_a_pressed()){
+        if (buttons.is_button_b_pressed()){
             file_index = (file_index + 1) % file_count;
-        } else if (buttons.is_button_b_pressed()){
+        } else if (buttons.is_button_a_pressed()){
             if (file_index == 0){
                 file_index = file_count - 1;
             } else {
                 file_index--;
             }
         } else if (buttons.is_button_x_pressed()){
+            if (file_count == 0){
+                printf("No files to delete\n");
+                return;
+            }
             printf("Deleting file %s\n", files[file_index].c_str());
 
             // Delete the file
@@ -91,16 +106,22 @@ void render_del_data(){
         }
 
         // Clear the screen
-        display.set_color(Colors::BLACK);
-        display.clear();
-        display.set_color(Colors::WHITE);
+        graphics.set_pen(Colors::BLACK);
+        graphics.clear();
+        graphics.set_pen(Colors::WHITE);
 
-        render_nav_arrows(110, Colors::WHITE);
-        render_items_list(file_index, files);
+        if (file_count == 0) graphics.text("No files to delete", Point{25, DISPLAY_HEIGHT/2 - 5}, 200, 2);
+        graphics.text("Delete file:", Point{25, 10}, 200, 2);
+        render_nav_arrows(2, Colors::WHITE);
+
+        graphics.set_pen(Colors::GREEN);
+        graphics.text("OK", Point{DISPLAY_WIDTH - 30, 20}, 200, 2);
+        graphics.set_pen(Colors::RED);
+        graphics.text("CANCEL", Point{DISPLAY_WIDTH - 65, DISPLAY_HEIGHT - 20}, 200, 2);
+        graphics.set_pen(Colors::WHITE);
+
+        render_items_list_full_page(file_index, files);
+
         driver.update(&graphics);
-
-        sleep_ms(200);
     }
-
-    del_data(selected_sensor);
 }
