66cadd57c872c8ba19affaf8651b8a52dcd97fb1
Screedy <screedy2k@gmail.com>
2024-07-01 17:20:08 +0200

    bug fixes, text alignment
    Added more sleeps to prevent unwanted button presses. Changed the wizard_start with initial functions. This was set incorrectly.

--- C++/pico-boilerplate/pages/format_sd.cpp
+++ C++/pico-boilerplate/pages/format_sd.cpp
@@ -25,17 +25,18 @@ bool render_format_sd(){
     graphics.text("Format SD card", pimoroni::Point{2, 0}, 236, 2);
     graphics.set_pen(Colors::RED);
     graphics.text("WARNING: This will delete all data on the SD card!", pimoroni::Point{2, 20}, 236, 2);
-    graphics.text("Press Y to confirm, X to cancel", Point{2, DISPLAY_HEIGHT - 20}, 236, 2);
+    graphics.text("Press Y to confirm\nPress X to cancel", Point{2, DISPLAY_HEIGHT - 40}, 236, 2);
     driver.update(&graphics);
 
+    sleep_ms(250);
     while(!buttons.is_button_y_pressed()){
+        sleep_ms(250);
         if (buttons.is_button_x_pressed()){
             return false;
         }
-        sleep_ms(200);
     }
 
     sd_card_manager->format();
-    wizard_start();
+    initial();
     return true;
 }
