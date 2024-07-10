ff39316b5e95105f81c0e7bd3dcd3e86d06e31d5
Screedy <screedy2k@gmail.com>
2024-07-10 16:57:23 +0200

    creation of string_modifiers
    A file that is dedicated to every function in this OS that changes strings behavior.

---
+++ C++/pico-boilerplate/utils/string_modifiers.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Petr Jurásek on 10.07.2024.
+//
+
+#include "string_modifiers.h"
+
+#include <sstream>
+
+// Function to split a string by a delimiter
+std::vector<std::string> split(const std::string &s, char delimiter) {
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(s);
+    while (std::getline(tokenStream, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
---
+++ C++/pico-boilerplate/utils/string_modifiers.h
@@ -0,0 +1,13 @@
+//
+// Created by Petr Jurásek on 10.07.2024.
+//
+
+#ifndef PICO_BOILERPLATE_STRING_MODIFIERS_H
+#define PICO_BOILERPLATE_STRING_MODIFIERS_H
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> split(const std::string &s, char delimiter);
+
+#endif //PICO_BOILERPLATE_STRING_MODIFIERS_H
