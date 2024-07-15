275bc792fff0318794246373fc74f7ffe2c7a395
Screedy <screedy2k@gmail.com>
2024-07-15 22:17:56 +0200

    Added buttons, changes to import

--- MicroPython/pages/add_sensor.py
+++ MicroPython/pages/add_sensor.py
@@ -1,8 +1,9 @@
 import time
-from config.config import *
+from config.config import Colors, Display, button_a, button_b, button_x, button_y
 from sensors.sensor_manager import SensorManager, AvailableSensors
 import page_elements
 from sensors.dht11 import DHT11
+from graphics.graphics import draw_next, draw_confirm, draw_cancel
 
 
 def render_add_sensor():
@@ -50,6 +51,7 @@ def select_sensor_type():
     selected_sensor = 0
 
     while True:
+        time.sleep(.15)
         if button_a.read():
             selected_sensor = (selected_sensor + 1) % sensor_list_length
 
@@ -64,9 +66,10 @@ def select_sensor_type():
 
         page_elements.clear_fast()
         display().set_pen(Colors.WHITE)
-        display().text(f"Select the sensor type: {sensor_list[selected_sensor]}", 2, 20, 200, 2)
+        display().text(f"Select the sensor type: {sensor_list[selected_sensor]}", 2, 20, 170, 2)
+        draw_next()
+        draw_cancel()
         display().update()
-        time.sleep(.15)
 
 
 def select_sensor_pin():
@@ -82,6 +85,7 @@ def select_sensor_pin():
     selected_pin = 0
 
     while True:
+        time.sleep(.15)
         if button_a.read():
             selected_pin = (selected_pin + 1) % pin_list_length
 
@@ -96,9 +100,10 @@ def select_sensor_pin():
 
         page_elements.clear_fast()
         display().set_pen(Colors.WHITE)
-        display().text(f"Select the sensor pin: {pin_list[selected_pin]}", 2, 20, 236, 2)
+        display().text(f"Select the sensor pin: {pin_list[selected_pin]}", 2, 20, 180, 2)
+        draw_next()
+        draw_cancel()
         display().update()
-        time.sleep(.15)
 
 
 def select_sensor_name():
@@ -113,6 +118,7 @@ def select_sensor_name():
     current_ascii = 65
 
     while True:
+        time.sleep(.15)
         if button_a.read():
             current_ascii = (current_ascii + 1) % 91
             if current_ascii == 0:
@@ -135,13 +141,14 @@ def select_sensor_name():
 
         page_elements.clear_fast()
         display().set_pen(Colors.WHITE)
-        display().text(f"Name of the sensor:", 2, 20, 236, 2)
+        display().text(f"Name of the sensor:", 2, 20, 180, 2)
         display().text(name, 2, 40, 236, 2)
         length = display().measure_text(name, 2) + 2
         display().text(chr(current_ascii), length, 40, 236, 2)
+        draw_next()
+        draw_cancel()
 
         display().update()
-        time.sleep(.15)
 
 
 def select_interval():
@@ -156,6 +163,7 @@ def select_interval():
     display().set_pen(Colors.WHITE)
 
     while True:
+        time.sleep(.15)
         if button_a.read():
             interval_time += 1
 
@@ -170,14 +178,16 @@ def select_interval():
 
         page_elements.clear_fast()
         display().set_pen(Colors.WHITE)
-        display().text(f"Select the refresh interval of the sensor", 2, 20, 236, 2)
-        display().text(f"Interval: {interval_time} m", 2, 40, 236, 2)
+        display().text(f"Select the refresh interval of the sensor", 2, 20, 150, 2)
+        display().text(f"Interval: {interval_time} m", 2, 80, 236, 2)
+        draw_confirm()
+        draw_cancel()
         display().update()
-        time.sleep(.15)
 
 
 if __name__ == "__main__":
     # select_sensor_name()
     # select_sensor_pin()
     # select_sensor_type()
+    # select_interval()
     render_add_sensor()
