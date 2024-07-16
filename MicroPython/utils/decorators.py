fb72feadba3cf062b7f9ff2f0a83ae85e90c1f78
Screedy <screedy2k@gmail.com>
2024-07-16 18:25:21 +0200

    Ram usage decorator

--- MicroPython/utils/decorators.py
+++ MicroPython/utils/decorators.py
@@ -1,4 +1,5 @@
 import time
+import gc
 
 
 def execution_time(func):
@@ -10,6 +11,22 @@ def execution_time(func):
         end = time.time()
 
         print(f"Execution time of {func.__name__} is {end-start:.8f}s")
+        return result
+
+    return wrapper
+
+
+def ram_usage(func):
+    """Decorator to measure the RAM usage of a function."""
+
+    def wrapper(*args, **kwargs):
+        gc.collect()                # Collect garbage before running the function
+        start_mem = gc.mem_free()   # Get the free memory before running the function
+        result = func(*args, **kwargs)
+        end_mem = gc.mem_free()     # Get the free memory after running the function
+
+        print(f"RAM usage before {func.__name__}: {start_mem} bytes\n RAM usage after {func.__name__}: {end_mem}.")
+        print(f"The difference was {start_mem - end_mem} bytes.")
         return result
 
     return wrapper
