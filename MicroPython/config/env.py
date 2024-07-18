c0dad83113a8e6dc5b2cb2a7d32d402bdba52764
Screedy <screedy2k@gmail.com>
2024-07-18 18:54:44 +0200

    Addition of TEST_GRAPH_MEMORY env variable

--- MicroPython/config/env.py
+++ MicroPython/config/env.py
@@ -2,5 +2,6 @@ env_vars = {
     'TEST_MEASUREMENT': False,
     'TEST_HOMEPAGE': False,
     'TEST_HOMEPAGE_MEMORY': False,
-    'TEST_GRAPH': True
+    'TEST_GRAPH': False,
+    'TEST_GRAPH_MEMORY': True,
 }
