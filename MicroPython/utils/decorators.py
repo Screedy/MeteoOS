1a22895e2f21394fb4d8efaa81e9094e03657ff8
Screedy <screedy2k@gmail.com>
2024-07-18 21:11:08 +0200

    Changes to imports
    Instead of importing whole libraries now only importing functions that are needed

--- MicroPython/utils/decorators.py
+++ MicroPython/utils/decorators.py
@@ -1,15 +1,15 @@
-import utime
-import gc
+from gc import collect, mem_free
+from utime import ticks_us, ticks_diff
 
 
 def execution_time(func):
     """Decorator to measure the execution time of a function."""
 
     def wrapper(*args, **kwargs):
-        start = utime.ticks_us()
+        start = ticks_us()
         result = func(*args, **kwargs)
-        end = utime.ticks_us()
-        exec_time = utime.ticks_diff(end, start) / 1_000_000    # Convert to seconds
+        end = ticks_us()
+        exec_time = ticks_diff(end, start) / 1_000_000    # Convert to seconds
 
         print(f"Execution time of {func.__name__} is {exec_time:.5f} seconds.")
         return result
@@ -21,10 +21,10 @@ def ram_usage(func):
     """Decorator to measure the RAM usage of a function."""
 
     def wrapper(*args, **kwargs):
-        gc.collect()                # Collect garbage before running the function
-        start_mem = gc.mem_free()   # Get the free memory before running the function
+        collect()                # Collect garbage before running the function
+        start_mem = mem_free()   # Get the free memory before running the function
         result = func(*args, **kwargs)
-        end_mem = gc.mem_free()     # Get the free memory after running the function
+        end_mem = mem_free()     # Get the free memory after running the function
 
         print(f"RAM usage before {func.__name__}: {start_mem} bytes\n RAM usage after {func.__name__}: {end_mem}.")
         print(f"The difference was {start_mem - end_mem} bytes.")
