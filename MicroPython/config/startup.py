b93a63c4488d7b6fc60329e0edd33095ab5892f9
Screedy <screedy2k@gmail.com>
2024-07-18 21:14:39 +0200

    More verbal boot sequence

--- MicroPython/config/startup.py
+++ MicroPython/config/startup.py
@@ -9,6 +9,8 @@ def startup():
     """First function to run on boot. Initializes the system and checks if the settings file exists.
     If not,  initialize setup."""
 
+    print("Initializing boot sequence")
+
     try:
         f = open("settings.txt", "r")   # Check if the settings file exists
         f.close()
@@ -18,6 +20,16 @@ def startup():
     sd = SDCardManager()
     sd.mount()
 
+    print("SD card mounted")
+
     settings = SettingsManager()    # Initialize the settings manager
+
+    print("Settings loaded")
+
     sensor_manager = SensorManager()    # Initialize the sensor manager
+
+    print("Sensors loaded")
+
     graph = ContextGraphInterval()    # Initialize the graph_strategy interval
+
+    print("Graph loaded")
