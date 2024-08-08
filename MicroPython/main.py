c659f96052aebde26c955f797510d814c1b841df
Screedy <screedy2k@gmail.com>
2024-08-08 15:15:58 +0200

    Trimming down main
    - Removed the render_homepage function. It is now in its own separate file.
    - redone what is in the main task. Now the handle_interrupt is in a second thread instead of the main_task.

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,4 +1,4 @@
-from utime import sleep, ticks_diff, ticks_us
+from utime import sleep, ticks_diff, ticks_us, localtime
 from _thread import start_new_thread
 from gc import collect, mem_free
 
@@ -7,101 +7,78 @@ from config.config import Colors, Display, GraphInterval
 from config.buttons import button_x, button_y, button_a, button_b
 from config.startup import startup
 from config.env import env_vars
-from graphics.graph_strategy.context_graph_interval import ContextGraphInterval
 from graphics import page_elements
 import pages.settings as settings
+from pages.homepage import render_homepage
 from sensors.sensor_manager import SensorManager
+from utils.misc_utils import handle_interrupt
+import machine
 
 
-def render_homepage(graph_interval):
-    """Renders the homepage with information about the selected sensor and selected graph_strategy interval.
-
-    :param graph_interval: The selected graph_strategy interval. Either 1 (Hourly), 2 (Daily), 3 (Weekly) or 4 (Monthly).
-    :return: None
-    """
-
-    display = Display()
-    graph = ContextGraphInterval()
-    sensor_manager = SensorManager()
-
-    page_elements.clear_fast()
-    display().set_pen(Colors.WHITE)
-
-    page_elements.render_homepage_buttons(graph_interval)
-    page_elements.render_nav_arrows(6)
-    page_elements.render_sensor_details()
-    display().line(101, 16, 101, 120, 3)
-
-    active_sensor = sensor_manager.sensors[sensor_manager.active_sensor]
-    # Value is fixed here to test on same data every time.
-    graph.render_graph((2024, 2, 15, 3), active_sensor, False)
-
-    display().update()
-
-    if env_vars['TEST_HOMEPAGE_MEMORY']:
-        return mem_free()
+stop_flag = False
 
 
 def main_task():
     """The main task that runs in the background."""
-
+    global stop_flag
     graph_interval = GraphInterval.Daily
     sensor_manager = SensorManager()
 
-    while True:     # Homepage loop
-        if button_x.read():
-            if graph_interval is GraphInterval.Daily:
-                graph_interval = GraphInterval.Weekly
-            elif graph_interval is GraphInterval.Weekly:
-                graph_interval = GraphInterval.Monthly
-            elif graph_interval is GraphInterval.Monthly:
-                graph_interval = GraphInterval.Daily
+    while not stop_flag:  # Homepage loop
+        try:
+            if button_x.read():
+                if graph_interval is GraphInterval.Daily:
+                    graph_interval = GraphInterval.Weekly
+                elif graph_interval is GraphInterval.Weekly:
+                    graph_interval = GraphInterval.Monthly
+                elif graph_interval is GraphInterval.Monthly:
+                    graph_interval = GraphInterval.Daily
+            elif button_y.read():
+                settings.settings_loop()
+            elif button_a.is_held():
+                sensor_manager.next_sensor()
+            elif button_b.is_held():
+                sensor_manager.previous_sensor()
 
-            print("Graph interval changed")
+            if env_vars['TEST_HOMEPAGE']:
+                start = ticks_us()
+                render_homepage(graph_interval)
+                end = ticks_us()
+                diff = ticks_diff(end, start) / 1_000_000
 
-        if button_y.read():
-            print("Opening settings")
-            settings.settings_loop()
-            print("Settings closed, back to homepage")
+                display = Display()
+                page_elements.clear_fast()
+                display().set_pen(Colors.RED)
+                display().text(f"Render time: {diff:.5f} s", 2, 2, 236, 2)
+                display().update()
+                sleep(2)
+            elif env_vars['TEST_HOMEPAGE_MEMORY']:
+                before_gc_collect = mem_free()
+                collect()
+                start_mem = mem_free()
+                end_mem = render_homepage(graph_interval)
+                diff_mem = start_mem - end_mem
 
-        if button_a.is_held():
-            sensor_manager.next_sensor()
+                display = Display()
+                page_elements.clear_fast()
+                display().set_pen(Colors.RED)
+                display().text(f"Memory before: {start_mem} B", 2, 2, 236, 2)
+                display().text(f"Memory after: {end_mem} B", 2, 20, 236, 2)
+                display().text(f"Memory diff: {diff_mem} B", 2, 40, 236, 2)
+                display().text(f"Bef collect: {before_gc_collect} B", 2, 60, 236, 2)
+                display().update()
+                sleep(2)
+            else:
+                render_homepage(graph_interval)
 
-        if button_b.is_held():
-            sensor_manager.previous_sensor()
-
-        if env_vars['TEST_HOMEPAGE']:
-            start = ticks_us()
-            render_homepage(graph_interval)
-            end = ticks_us()
-            diff = ticks_diff(end, start) / 1_000_000
-
-            display = Display()
-            page_elements.clear_fast()
-            display().set_pen(Colors.RED)
-            display().text(f"Render time: {diff:.5f} s", 2, 2, 236, 2)
-            display().update()
-            sleep(2)
-        elif env_vars['TEST_HOMEPAGE_MEMORY']:
-            before_gc_collect = mem_free()
-            collect()
-            start_mem = mem_free()
-            end_mem = render_homepage(graph_interval)
-            diff_mem = start_mem - end_mem
-
-            display = Display()
-            page_elements.clear_fast()
-            display().set_pen(Colors.RED)
-            display().text(f"Memory before: {start_mem} B", 2, 2, 236, 2)
-            display().text(f"Memory after: {end_mem} B", 2, 20, 236, 2)
-            display().text(f"Memory diff: {diff_mem} B", 2, 40, 236, 2)
-            display().text(f"Bef collect: {before_gc_collect} B", 2, 60, 236, 2)
-            display().update()
-            sleep(2)
-        else:
-            render_homepage(graph_interval)
-
-        sleep(0.1)
+            sleep(0.1)
+        except KeyboardInterrupt:
+            stop_flag = True  # Set the flag to stop the background task
+            sleep(1)  # Allow some time for the background task to notice the flag
+            machine.soft_reset()  # Perform a soft reset
+            break
+        except Exception as e:
+            print("Error in main_task:", e)
 
 
 if __name__ == "__main__":
@@ -120,4 +97,9 @@ if __name__ == "__main__":
     fr_homepage.close()
 
     collect()
-    start_new_thread(main_task, ())
+    try:
+        start_new_thread(handle_interrupt, ())
+    except Exception as e:
+        print("Error starting main_task thread:", e)
+
+    main_task()
